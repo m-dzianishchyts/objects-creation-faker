b532ce56b66dc87ca1d873044a7b4d12c3035464
maxiemar <denishicmihail@gmail.com>
2021-10-26 14:59:28 +0300

    test: add faker tests

---
+++ Faker.Test/Data/Data.cs
@@ -0,0 +1,269 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Faker.Test.Data
+{
+    #region Faker instantiates class
+
+    public class A1
+    {
+    }
+
+    #endregion
+
+    #region Faker prefers a constructor with a larger number of parameters
+
+    public class A2
+    {
+        public const int ONE_PARAMETER_CONSTRUCTOR_FIELD_VALUE = 13;
+        public const int PARAMETERLESS_CONSTRUCTOR_FIELD_VALUE = 42;
+
+        public int field;
+
+        public A2(int anyParameter)
+        {
+            field = ONE_PARAMETER_CONSTRUCTOR_FIELD_VALUE;
+        }
+
+        public A2()
+        {
+            field = PARAMETERLESS_CONSTRUCTOR_FIELD_VALUE;
+        }
+    }
+
+    #endregion
+
+    #region Faker can use private constructor
+
+    public class A3
+    {
+        private A3()
+        {
+        }
+    }
+
+    #endregion
+
+    #region Faker prefers public constructor
+
+    public class A4
+    {
+        public const int PUBLIC_CONSTRUCTOR_FIELD_VALUE = 12;
+        public const int PRIVATE_CONSTRUCTOR_FIELD_VALUE = 99;
+
+        public int field;
+
+        public A4(int anyParameter)
+        {
+            field = PUBLIC_CONSTRUCTOR_FIELD_VALUE;
+        }
+
+        private A4(float anyParameter)
+        {
+            field = PRIVATE_CONSTRUCTOR_FIELD_VALUE;
+        }
+    }
+
+    #endregion
+
+    #region Faker initializes constructor paremeters
+
+    public class A5
+    {
+        public B5 field;
+
+        public A5(B5 field)
+        {
+            this.field = field;
+        }
+    }
+
+    public class B5
+    {
+    }
+
+    #endregion
+
+    #region Faker initializes fields
+
+    public class A6
+    {
+        public B6 field;
+    }
+
+    public class B6
+    {
+    }
+
+    #endregion
+
+    #region Faker initializes properties
+
+    public class A7
+    {
+        public B7? Property { get; set; }
+    }
+
+    public class B7
+    {
+    }
+
+    #endregion
+
+    #region Faker ignores non-public fields
+
+    public class A8
+    {
+        private B8 privateField;
+        protected B8 protectedField;
+        internal B8 internalField;
+        protected internal B8 protectedInternalField;
+        private protected B8 privateProtectedField;
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsPrivateFieldInitialized()
+        {
+            return privateField is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsProtectedFieldInitialized()
+        {
+            return protectedField is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsInternalFieldInitialized()
+        {
+            return internalField is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsProtectedInternalFieldInitialized()
+        {
+            return protectedInternalField is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsPrivateProtectedFieldInitialized()
+        {
+            return privateProtectedField is null;
+        }
+    }
+
+    public class B8
+    {
+    }
+
+    #endregion
+
+    #region Faker ignores non-public properties
+    
+    public class A9
+    {
+        private B9? PrivateProperty { get; set; }
+        protected B9? ProtectedProperty { get; set; }
+        internal B9? InternalProperty { get; set; }
+        protected internal B9? ProtectedInternalProperty { get; set; }
+        private protected B9? PrivateProtectedProperty { get; set; }
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsPrivatePropertyInitialized()
+        {
+            return PrivateProperty is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsProtectedPropertyInitialized()
+        {
+            return ProtectedProperty is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsInternalPropertyInitialized()
+        {
+            return InternalProperty is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsProtectedInternalPropertyInitialized()
+        {
+            return ProtectedInternalProperty is null;
+        }
+        
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public bool IsPrivateProtectedPropertyInitialized()
+        {
+            return PrivateProtectedProperty is null;
+        }
+    }
+
+    public class B9
+    {
+    }
+
+    #endregion
+
+    #region Faker ignores initialized fields
+
+    public class A10
+    {
+        public const int FIELD_INITIALIZATION_VALUE = 13;
+        public int initializedField = FIELD_INITIALIZATION_VALUE;
+    }
+
+    #endregion
+
+    #region Faker ignores initialized properties
+
+    public class A11
+    {
+        public const int PROPERTY_INITIALIZATION_VALUE = 23;
+        public int Property { get; set; } = PROPERTY_INITIALIZATION_VALUE;
+    }
+
+    #endregion
+
+    #region Faker instantiates structs
+
+    public struct A12
+    {
+    }
+
+    #endregion
+
+    #region Faker fails on shallow cyclic dependency
+
+    public class A13
+    {
+        public A13(A13 cyclicDependency)
+        {
+        }
+    }
+
+    #endregion
+    
+    #region Faker fails on deep cyclic dependency
+
+    public class A14
+    {
+        public A14(B14 dependency)
+        {
+        }
+    }
+
+    public class B14
+    {
+        public B14(C14 dependency)
+        {
+        }
+    }
+
+    public class C14
+    {
+        public C14(B14 cyclicDependency)
+        {
+        }
+    }
+
+    #endregion
+}
---
+++ Faker.Test/FakerTest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Faker.Core;
+using Faker.Core.Exception;
+using Faker.Test.Data;
+using NUnit.Framework;
+
+namespace Faker.Test
+{
+    [TestFixture]
+    public class FakerTest
+    {
+        private readonly IFaker _faker;
+
+        public FakerTest()
+        {
+            _faker = new Core.Faker();
+        }
+
+        private static TestCaseData[] s_availableTypes =
+        {
+            new(typeof(bool)),
+            new(typeof(byte)),
+            new(typeof(sbyte)),
+            new(typeof(char)),
+            new(typeof(decimal)),
+            new(typeof(double)),
+            new(typeof(float)),
+            new(typeof(int)),
+            new(typeof(uint)),
+            new(typeof(long)),
+            new(typeof(ulong)),
+            new(typeof(short)),
+            new(typeof(ushort)),
+
+            new(typeof(string)),
+            new(typeof(DateTime)),
+            new(typeof(List<int>))
+        };
+
+        private static TestCaseData[] s_unavailableTypes =
+        {
+            new(typeof(BigInteger)),
+            new(typeof(Uri)),
+            new(typeof(nint)),
+            new(typeof(nuint))
+        };
+
+        [Test]
+        [TestCaseSource(nameof(s_availableTypes))]
+        public void Faker_Create_AvailableTypes_DoesNotThrow(Type type)
+        {
+            MethodInfo genericMethodCreate = MakeGenericMethodCreate(type);
+            Assert.DoesNotThrow(() => genericMethodCreate.Invoke(_faker, Array.Empty<object>()));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(s_availableTypes))]
+        public void Faker_Creates_ExpectedType(Type type)
+        {
+            MethodInfo genericMethodCreate = MakeGenericMethodCreate(type);
+            Assert.AreEqual(type, genericMethodCreate.Invoke(_faker, Array.Empty<object>())?.GetType());
+        }
+
+        [Test]
+        [TestCaseSource(nameof(s_unavailableTypes))]
+        public void Faker_Create_UnavailableTypes_Throw(Type type)
+        {
+            MethodInfo genericMethodCreate = MakeGenericMethodCreate(type);
+            Assert.Throws(Is.InstanceOf(typeof(ApplicationException)), () =>
+            {
+                genericMethodCreate.Invoke(_faker, Array.Empty<object>());
+            });
+        }
+
+        [Test]
+        public void Faker_Create_Class_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => _faker.Create<A1>());
+        }
+        
+        [Test]
+        public void Faker_Create_Class_NotNull()
+        {
+            A1 obj = _faker.Create<A1>();
+            Assert.IsNotNull(obj);
+        }
+
+        [Test]
+        public void Faker_Prefers_Constructor_WithLargerNumberOfParams()
+        {
+            A2 obj = _faker.Create<A2>();
+            Assert.AreEqual(A2.ONE_PARAMETER_CONSTRUCTOR_FIELD_VALUE ,obj.field);
+        }
+
+        [Test]
+        public void Faker_Create_WithPrivateConstructor_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => _faker.Create<A3>());
+        }
+
+        [Test]
+        public void Faker_Prefers_PublicConstructor()
+        {
+            A4 obj = _faker.Create<A4>();
+            Assert.AreEqual(A4.PUBLIC_CONSTRUCTOR_FIELD_VALUE, obj.field);
+        }
+
+        [Test]
+        public void Faker_Initializes_ConstructorParameters()
+        {
+            A5 obj = _faker.Create<A5>();
+            Assert.IsNotNull(obj.field);
+        }
+
+        [Test]
+        public void Faker_Initializes_Fields()
+        {
+            A6 obj = _faker.Create<A6>();
+            Assert.IsNotNull(obj.field);
+        }
+
+        [Test]
+        public void Faker_Initialized_Properties()
+        {
+            A7 obj = _faker.Create<A7>();
+            Assert.IsNotNull(obj.Property);
+        }
+
+        [Test]
+        public void Faker_Ignores_NonPublicFields()
+        {
+            A8 obj = _faker.Create<A8>();
+            Assert.IsTrue(obj.IsPrivateFieldInitialized());
+            Assert.IsTrue(obj.IsProtectedFieldInitialized());
+            Assert.IsTrue(obj.IsInternalFieldInitialized());
+            Assert.IsTrue(obj.IsProtectedInternalFieldInitialized());
+            Assert.IsTrue(obj.IsPrivateProtectedFieldInitialized());
+        }
+        
+        [Test]
+        public void Faker_Ignores_NonPublicProperties()
+        {
+            A9 obj = _faker.Create<A9>();
+            Assert.IsTrue(obj.IsPrivatePropertyInitialized());
+            Assert.IsTrue(obj.IsProtectedPropertyInitialized());
+            Assert.IsTrue(obj.IsInternalPropertyInitialized());
+            Assert.IsTrue(obj.IsProtectedInternalPropertyInitialized());
+            Assert.IsTrue(obj.IsPrivateProtectedPropertyInitialized());
+        }
+
+        [Test]
+        public void Faker_Ignores_InitializedFields()
+        {
+            A10 obj = _faker.Create<A10>();
+            Assert.AreEqual(A10.FIELD_INITIALIZATION_VALUE, obj.initializedField);
+        }
+
+        [Test]
+        public void Faker_Ignores_InitializedProperties()
+        {
+            A11 obj = _faker.Create<A11>();
+            Assert.AreEqual(A11.PROPERTY_INITIALIZATION_VALUE, obj.Property);
+        }
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        public void Faker_Create_Struct_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => _faker.Create<A12>());
+        }
+
+        [Test]
+        public void Faker_Create_WithShallowCyclicDependency_Throws()
+        {
+            Assert.Throws<CyclicDependencyException>(() => _faker.Create<A13>());
+        }
+        
+        [Test]
+        public void Faker_Create_WithDeepCyclicDependency_Throws()
+        {
+            Assert.Throws<CyclicDependencyException>(() => _faker.Create<A14>());
+        }
+        
+        private MethodInfo MakeGenericMethodCreate(Type type)
+        {
+            MethodInfo methodCreate = _faker.GetType().GetMethod(nameof(Core.Faker.Create))!;
+            MethodInfo genericMethodCreate = methodCreate.MakeGenericMethod(type);
+            return genericMethodCreate;
+        }
+    }
+}
